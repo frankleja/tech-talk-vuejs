4e3878657f5aa94bf681c32d62ee609e5c89815c
Frank Leja <Frank.Leja@msg.group>
2019-01-20 17:14:02 +0100

    attr and style binding added

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -3,6 +3,15 @@
         <h2>Data Binding</h2>
         <p>current time is {{timestamp}}</p>
         <p>date is {{date}}</p>
+
+        <h3>Attribute binding</h3>
+        <a v-bind:href="linkUrl">{{linkText}}</a>
+
+        <h3>Style class binding</h3>
+        <p class="fett" v-bind:class="{ big: isBig, red: isRed }">ein Beispieltext</p>
+
+        <button v-on:click="isBig ? isBig=false : isBig=true">toggle size</button>
+        <button v-on:click="isRed ? isRed=false : isRed=true">toggle red</button>
     </div>
 </template>
 
@@ -12,7 +21,11 @@ export default {
      // observable data 
     data () {
         return {
-            timestamp: null
+            timestamp: null,
+            linkUrl: 'http://www.heise.de/',
+            linkText: 'Heise',
+            isBig: true,
+            isRed: false
         }
     }, 
     // derived data
@@ -33,3 +46,16 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+    .big {
+        font-size: 20px;
+    }
+    .red {
+        color: red;
+    }
+    .fett {
+        font-weight: bolder;
+    }
+</style>
+
